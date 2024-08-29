06eb0c43d10e8628ae090d5e4cd4b84b57262525
Marcelo <marcelo.p.moro@hotmail.com>
2024-08-29 19:50:29 -0300

    Criacao da classe posicao e da classe main

---
+++ src/Aplicacao/Main.java
@@ -0,0 +1,12 @@
+package Aplicacao;
+
+import Tabuleiro.Posicao;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Posicao p1 = new Posicao(1, 2);
+		System.out.println(p1.toString());
+	}
+
+}
